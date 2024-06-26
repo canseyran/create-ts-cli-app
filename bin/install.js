0bf11b8babd3fbcf2117da0fe4969952d9d8ec29
Ogulcan Seyran <seyran.ogulcan@gmail.com>
2024-06-26 11:33:33 +0200

    Improve cli installation output

--- bin/install.js
+++ bin/install.js
@@ -3,60 +3,55 @@
 import * as fs from "fs";
 import * as path from "path";
 import chalk from "chalk";
-import { spawn } from "node:child_process";
-import packageJson from "../template/package.json" with {type: "json"};
 import { spawnSync } from "child_process";
+import packageJson from "../template/package.json" with {type: "json"};
 
 const CURRENT_DIRECTORY_IDENTIFIER = ".";
 const TEMPLATE_FOLDER_NAME = "template";
 const PATHS_TO_EXCLUDE = ["node_modules", "dist"];
 const DIRECTORY_VALIDATION_REGEXP = /^[^\s^\x00-\x1f\\?*:"";<>|\/.][^\x00-\x1f\\?*:"";<>|\/]*[^\s^\x00-\x1f\\?*:"";<>|\/.]+$/;
-const ERROR_MESSAGES = {
-  MISSING_PROJECT_DIRECTORY: `Please specify the project directory:\n ${chalk.blue("create-ts-cli-app")} ${chalk.red("<project-directory>")}\n\nExamples:\n ${chalk.blue("create-ts-cli-app")} ${chalk.red("my-cli-app")}\n ${chalk.blue("create-ts-cli-app")} ${chalk.red(".")}`,
-  INVALID_PROJECT_DIRECTORY: `Please specify a valid directory name`,
-  DIRECTORY_ALREADY_EXISTS: `Specified directory already exists.\nMake sure to provide an used project directory`
-}
 const GREEN_CHECKMARK = chalk.green("✔")
-const STATUS_MESSAGES = {
-  START: `---- Generating Typescript NodeJS CLI App ----`,
-  COPYING_FILES: `Copying files ...`,
-  FILES_COPIED: `Files copied ${GREEN_CHECKMARK}`,
-  NPM_INSTALL: `Installing packages ...`,
-  PACKAGES_INSTALLED: `Packages installed ${GREEN_CHECKMARK}`,
-  COMPLETE: `Completed ${GREEN_CHECKMARK}\nRun:\n cd <project-directory>\n npm run dev\n\nTo start developing!`,
+
+const ErrorHandlers = {
+  MISSING_PROJECT_DIRECTORY: () => {
+    console.log(`Please specify the project directory:\n ${chalk.blue("create-ts-cli-app")} ${chalk.red("<project-directory>")}\n\nExamples:\n ${chalk.blue("create-ts-cli-app")} ${chalk.red("my-cli-app")}\n ${chalk.blue("create-ts-cli-app")} ${chalk.red(".")}`)
+    console.exit(1);
+  },
+  INVALID_PROJECT_DIRECTORY: () => { console.log(`Please specify a valid directory name`); console.exit(1); },
+  DIRECTORY_ALREADY_EXISTS: () => { console.log(`Specified directory already exists.\nMake sure to provide an used project directory`); console.exit(1); }
 }
 
-function logErrorAndExit(errorMsg) {
-  console.log(errorMsg)
-  console.exit(1);
+const StatusMessages = {
+  START: () => console.log(`---- Generating Typescript NodeJS CLI App ----`),
+  COPYING_FILES: () => console.log(`Copying files ...`),
+  FILES_COPIED: (path) => console.log(`Files copied to ${path} ${GREEN_CHECKMARK}`),
+  NPM_INSTALL: () => console.log(`Installing packages ...`),
+  PACKAGES_INSTALLED: () => console.log(`Packages installed ${GREEN_CHECKMARK}`),
+  COMPLETE: (directory) => console.log(`Completed ${GREEN_CHECKMARK}\nChange into project directory:\n ${chalk.red("cd")} ${chalk.blue(directory)}\nStart in watch mode:\n ${chalk.red("npm run")} ${chalk.blue("dev")}\nUnit testing in watch mode:\n ${chalk.red("npm run")} ${chalk.blue("test:watch")}\nBuild a single file executable node script:\n ${chalk.red("npm run")} ${chalk.blue("build:script")}\n\nCheck package.json for additional scripts, happy coding!`),
 }
 
-function logStatusMessage(statusMsg) {
-  console.log(statusMsg);
-}
-
-logStatusMessage(STATUS_MESSAGES.START);
+StatusMessages.START();
 
 const projectDirectoryArg = process.argv[2];
 
 if (!projectDirectoryArg) {
-  logErrorAndExit(ERROR_MESSAGES.MISSING_PROJECT_DIRECTORY)
+  ErrorHandlers.MISSING_PROJECT_DIRECTORY();
 }
 
 const isValidDirectoryName = DIRECTORY_VALIDATION_REGEXP.test(projectDirectoryArg);
 const useCurrentDirectory = projectDirectoryArg.trim() === CURRENT_DIRECTORY_IDENTIFIER;
 
 if (!isValidDirectoryName && !useCurrentDirectory) {
-  logErrorAndExit(ERROR_MESSAGES.INVALID_PROJECT_DIRECTORY)
+  ErrorHandlers.INVALID_PROJECT_DIRECTORY();
 }
 
 const projectDirectoryAbsPath = useCurrentDirectory ? process.cwd() : path.join(process.cwd(), projectDirectoryArg);
 
 if (!useCurrentDirectory && fs.existsSync(projectDirectoryAbsPath)) {
-  logErrorAndExit(ERROR_MESSAGES.DIRECTORY_ALREADY_EXISTS);
+  ErrorHandlers.DIRECTORY_ALREADY_EXISTS();
 }
 
-logStatusMessage(STATUS_MESSAGES.COPYING_FILES);
+StatusMessages.COPYING_FILES();
 
 if (!useCurrentDirectory) {
   fs.mkdirSync(projectDirectoryAbsPath, { recursive: true });
@@ -76,12 +71,12 @@ const packageJsonAbsPath = path.join(projectDirectoryAbsPath, "package.json");
 
 fs.writeFileSync(packageJsonAbsPath, JSON.stringify(packageJson, undefined, 2))
 
-logStatusMessage(STATUS_MESSAGES.FILES_COPIED);
+StatusMessages.FILES_COPIED(projectDirectoryAbsPath);
 
-logStatusMessage(STATUS_MESSAGES.NPM_INSTALL);
+StatusMessages.NPM_INSTALL();
 
 spawnSync("npm", ["install"], { stdio: "ignore", cwd: projectDirectoryAbsPath });
 
-logStatusMessage(STATUS_MESSAGES.PACKAGES_INSTALLED);
+StatusMessages.PACKAGES_INSTALLED()
 
-logStatusMessage(STATUS_MESSAGES.COMPLETE);
+StatusMessages.COMPLETE(projectDirectoryArg);
