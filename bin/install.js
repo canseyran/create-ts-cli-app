a9556e93c9af23f6ae3114c435f8c0ab859ad95f
Ogulcan Seyran <seyran.ogulcan@gmail.com>
2024-06-26 02:06:49 +0200

    Refactor installation script

--- bin/install.js
+++ bin/install.js
@@ -1,36 +1,87 @@
-#!/usr/bin/env node
-// Usage: npx create-my-template my-app
+#!/usr/bin/env node --no-warnings=ExperimentalWarning
 
-import { cp as copy, mkdirSync, readFileSync, writeFileSync, renameSync } from 'fs/promises';
-import path from 'path';
-import { cwd } from 'process';
-import { sync as spawnSync } from 'child_process';
+import * as fs from "fs";
+import * as path from "path";
+import chalk from "chalk";
+import { spawn } from "node:child_process";
+import packageJson from "../template/package.json" with {type: "json"};
+import { spawnSync } from "child_process";
 
-const projectName = process.argv[2];
+const CURRENT_DIRECTORY_IDENTIFIER = ".";
+const TEMPLATE_FOLDER_NAME = "template";
+const PATHS_TO_EXCLUDE = ["node_modules", "dist"];
+const DIRECTORY_VALIDATION_REGEXP = /^[^\s^\x00-\x1f\\?*:"";<>|\/.][^\x00-\x1f\\?*:"";<>|\/]*[^\s^\x00-\x1f\\?*:"";<>|\/.]+$/;
+const ERROR_MESSAGES = {
+  MISSING_PROJECT_DIRECTORY: `Please specify the project directory:\n ${chalk.blue("create-ts-cli-app")} ${chalk.red("<project-directory>")}\n\nExamples:\n ${chalk.blue("create-ts-cli-app")} ${chalk.red("my-cli-app")}\n ${chalk.blue("create-ts-cli-app")} ${chalk.red(".")}`,
+  INVALID_PROJECT_DIRECTORY: `Please specify a valid directory name`,
+  DIRECTORY_ALREADY_EXISTS: `Specified directory already exists.\nMake sure to provide an used project directory`
+}
+const GREEN_CHECKMARK = chalk.green("✔")
+const STATUS_MESSAGES = {
+  START: `---- Generating Typescript NodeJS CLI App ----`,
+  COPYING_FILES: `Copying files ...`,
+  FILES_COPIED: `Files copied ${GREEN_CHECKMARK}`,
+  NPM_INSTALL: `Installing packages ...`,
+  PACKAGES_INSTALLED: `Packages installed ${GREEN_CHECKMARK}`,
+  COMPLETE: `Completed ${GREEN_CHECKMARK}\nRun:\n cd <project-directory>\n npm run dev\n\nTo start developing!`,
+}
 
-const currentDir = cwd();
-const projectDir = path.resolve(currentDir, projectName);
-await mkdirSync(projectDir, { recursive: true });
+function logErrorAndExit(errorMsg) {
+  console.log(errorMsg)
+  console.exit(1);
+}
 
-const templateDir = path.resolve(new URL('template', import.meta.url).pathname);
-await copy(templateDir, projectDir, { recursive: true });
+function logStatusMessage(statusMsg) {
+  console.log(statusMsg);
+}
 
-await renameSync(
-  path.join(projectDir, 'gitignore'),
-  path.join(projectDir, '.gitignore')
-);
+logStatusMessage(STATUS_MESSAGES.START);
 
-const projectPackageJsonPath = path.join(projectDir, 'package.json');
-const projectPackageJson = JSON.parse(await readFileSync(projectPackageJsonPath, 'utf8'));
+const projectDirectoryArg = process.argv[2];
 
-projectPackageJson.name = projectName;
+if (!projectDirectoryArg) {
+  logErrorAndExit(ERROR_MESSAGES.MISSING_PROJECT_DIRECTORY)
+}
 
-await writeFileSync(
-  projectPackageJsonPath,
-  JSON.stringify(projectPackageJson, null, 2)
-);
+const isValidDirectoryName = DIRECTORY_VALIDATION_REGEXP.test(projectDirectoryArg);
+const useCurrentDirectory = projectDirectoryArg.trim() === CURRENT_DIRECTORY_IDENTIFIER;
 
-spawnSync('npm', ['install'], { stdio: 'inherit' });
+if (!isValidDirectoryName && !useCurrentDirectory) {
+  logErrorAndExit(ERROR_MESSAGES.INVALID_PROJECT_DIRECTORY)
+}
 
-console.log('Success! Your new project is ready.');
-console.log(`Created ${projectName} at ${projectDir}`);
+const projectDirectoryAbsPath = useCurrentDirectory ? process.cwd() : path.join(process.cwd(), projectDirectoryArg);
+
+if (!useCurrentDirectory && fs.existsSync(projectDirectoryAbsPath)) {
+  logErrorAndExit(ERROR_MESSAGES.DIRECTORY_ALREADY_EXISTS);
+}
+
+logStatusMessage(STATUS_MESSAGES.COPYING_FILES);
+
+if (!useCurrentDirectory) {
+  fs.mkdirSync(projectDirectoryAbsPath, { recursive: true });
+}
+
+const templateDirectoryAbsPath = path.join(import.meta.dirname, "..", TEMPLATE_FOLDER_NAME);
+
+fs.cpSync(templateDirectoryAbsPath, projectDirectoryAbsPath, {
+  recursive: true, filter: (path) => {
+    return !PATHS_TO_EXCLUDE.some((excludedPath) => path.includes(excludedPath));
+  }
+});
+
+packageJson.name = projectDirectoryArg;
+
+const packageJsonAbsPath = path.join(projectDirectoryAbsPath, "package.json");
+
+fs.writeFileSync(packageJsonAbsPath, JSON.stringify(packageJson, undefined, 2))
+
+logStatusMessage(STATUS_MESSAGES.FILES_COPIED);
+
+logStatusMessage(STATUS_MESSAGES.NPM_INSTALL);
+
+spawnSync("npm", ["install"], { stdio: "ignore", cwd: projectDirectoryAbsPath });
+
+logStatusMessage(STATUS_MESSAGES.PACKAGES_INSTALLED);
+
+logStatusMessage(STATUS_MESSAGES.COMPLETE);
